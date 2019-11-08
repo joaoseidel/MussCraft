940d0b7fb0a503dc4d9a93d885d663429d58be6b
Joao Seidel <joaovseidel@gmail.com>
2019-11-08 00:22:35 -0200

    Add MussPlayer entity

---
+++ src/main/java/models/MussPlayer.java
@@ -0,0 +1,105 @@
+package models;
+
+import com.dieselpoint.norm.serialize.DbSerializer;
+import models.serializers.LocationSerializer;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Table(name = "MussPlayer")
+public class MussPlayer extends AbstractBaseEntity {
+
+    @Transient
+    private Player player;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private Double money;
+
+    private Double experience;
+
+    private boolean logged;
+
+    private boolean registered;
+
+    @DbSerializer(LocationSerializer.class)
+    private Location location;
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    public Double getExperience() {
+        return experience;
+    }
+
+    public void setExperience(Double experience) {
+        this.experience = experience;
+    }
+
+    public boolean isLogged() {
+        return logged;
+    }
+
+    public void setLogged(boolean logged) {
+        this.logged = logged;
+    }
+
+    public boolean isRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(boolean registered) {
+        this.registered = registered;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+}
