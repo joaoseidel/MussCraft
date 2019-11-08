253183a02279c17a1b87fa81fc601e25ea260e99
Joao Seidel <joaovseidel@gmail.com>
2019-11-08 00:16:06 -0200

    Add HashHelper to generate Hashed strings Add LocationHelper to random location and get nearby blocks

---
+++ src/main/java/helpers/LocationHelper.java
@@ -0,0 +1,38 @@
+package helpers;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class LocationHelper {
+    public static Location getRandomLocationWithinRadius(Location location, int radius) {
+        Random random = new Random();
+        Location center = location.clone();
+        World locationWorld = center.getWorld();
+
+        double diameter = radius * 2;
+        double x = center.getX() + (random.nextDouble() * diameter - radius);
+        double z = center.getZ() + (random.nextDouble() * diameter - radius);
+
+        return new Location(
+                locationWorld, x,
+                locationWorld.getHighestBlockYAt(center), z);
+    }
+
+    public static List<Block> getNearbyBlocks(Location location, int radius) {
+        List<Block> blocks = new ArrayList<Block>();
+        for (int x = location.getBlockX() - radius; x <= location.getBlockX() + radius; x++) {
+            for (int y = location.getBlockY() - radius; y <= location.getBlockY() + radius; y++) {
+                for (int z = location.getBlockZ() - radius; z <= location.getBlockZ() + radius; z++) {
+                    blocks.add(location.getWorld().getBlockAt(x, y, z));
+                }
+            }
+        }
+        return blocks;
+    }
+
+}
