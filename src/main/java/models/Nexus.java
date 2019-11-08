d893b1bbf6c1133be69bc917e725e275320ace6b
Joao Seidel <joaovseidel@gmail.com>
2019-11-08 00:31:33 -0200

    Add Nexus entity

---
+++ src/main/java/models/Nexus.java
@@ -0,0 +1,115 @@
+package models;
+
+import org.bukkit.Location;
+import org.bukkit.boss.BossBar;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "Nexus")
+public class Nexus extends AbstractBaseEntity {
+
+    private String name;
+
+    private Double health;
+
+    private Integer power;
+
+    private Date destroyedDate;
+
+    private Location location;
+
+    private MussPlayer owner;
+
+    @Transient
+    private BossBar bossBar;
+
+    private boolean destroyed;
+
+    @Transient
+    private List<org.bukkit.entity.Entity> minions = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getHealth() {
+        return health;
+    }
+
+    public void setHealth(Double health) {
+        this.health = health;
+    }
+
+    public Integer getPower() {
+        return power;
+    }
+
+    public void setPower(Integer power) {
+        this.power = power;
+    }
+
+    public Date getDestroyedDate() {
+        return destroyedDate;
+    }
+
+    public void setDestroyedDate(Date destroyedDate) {
+        this.destroyedDate = destroyedDate;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public MussPlayer getOwner() {
+        return owner;
+    }
+
+    public void setOwner(MussPlayer owner) {
+        this.owner = owner;
+    }
+
+    public BossBar getBossBar() {
+        return bossBar;
+    }
+
+    public void setBossBar(BossBar bossBar) {
+        this.bossBar = bossBar;
+    }
+
+    public boolean isDestroyed() {
+        return destroyed;
+    }
+
+    public void setDestroyed(boolean destroyed) {
+        this.destroyed = destroyed;
+    }
+
+    public List<org.bukkit.entity.Entity> getMinions() {
+        return minions;
+    }
+
+    public void setMinions(List<org.bukkit.entity.Entity> minions) {
+        this.minions = minions;
+    }
+
+    @Override
+    public String toString() {
+        return super.getUuid()
+                .concat("@")
+                .concat(name);
+    }
+}
