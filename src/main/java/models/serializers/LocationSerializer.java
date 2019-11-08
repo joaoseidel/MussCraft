aa06652bacaa78b262c412f0327b80eedfe8c553
Joao Seidel <joaovseidel@gmail.com>
2019-11-08 00:17:23 -0200

    Add LocationSerializer to database serialization

---
+++ src/main/java/models/serializers/LocationSerializer.java
@@ -0,0 +1,25 @@
+package models.serializers;
+
+import com.dieselpoint.norm.serialize.DbSerializable;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+public class LocationSerializer implements DbSerializable {
+    public String serialize(Object location) {
+        return location.toString();
+    }
+
+    public Object deserialize(String serializedLocation, Class<?> aClass) {
+        String[] deserializedLocation = serializedLocation.split(",");
+        String worldName = deserializedLocation[0].split("name=")[1].replaceAll("}", "");
+
+        return new Location(
+                Bukkit.getWorld(worldName),
+                Double.parseDouble(deserializedLocation[1].split("=")[1]),
+                Double.parseDouble(deserializedLocation[2].split("=")[1]),
+                Double.parseDouble(deserializedLocation[3].split("=")[1]),
+                Float.parseFloat(deserializedLocation[4].split("=")[1]),
+                Float.parseFloat(deserializedLocation[5].split("=")[1])
+        );
+    }
+}
